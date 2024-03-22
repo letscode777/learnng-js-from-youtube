312572e2d80d0855fb5bb1344b0cac54a57e4f58
letscode777 <163330053+letscode777@users.noreply.github.com>
2024-03-22 12:47:51 +0000

    data type

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,6 @@
+"use strict" // treat all js code to newer version
+//(4+4) // we areusing node.js not browser
+console.log(4+
+    4) // code readability should be HIGH {ALTHOUGH ITS READABLE}
+    console.log(5+5) // this is uch better
+    console.log("CHANDAN")
