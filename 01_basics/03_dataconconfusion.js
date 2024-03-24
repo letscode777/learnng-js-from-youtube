b3520af574fdddb7d84207f0cade5edc6f91db37
letscode777 <163330053+letscode777@users.noreply.github.com>
2024-03-24 15:52:25 +0000

    changes conversion

---
+++ 01_basics/03_dataconconfusion.js
@@ -0,0 +1,24 @@
+//let score = "33abc"
+
+//console.log(typeof score ); // string
+//console.log(typeof(score)); // string
+
+//let scoreInNumber = Number(score) // string to number conversion
+//console.log(typeof(scoreInNumber))
+//console.log(typeof score)
+//console.log(scoreInNumber)   //  output  NaN (not a number)
+
+// output can be different if score has different data types { output will have value according to string boolean var const datatypes}
+  // "33" -> 33
+  // "33abc" -> NaN
+  // true-> 1 , false-> 0
+
+  let isLoggedIn = "hh"
+  let booleanisLoggedIn = Boolean(isLoggedIn) // * CAPTITAL LETTER EROR *
+  console.log(booleanisLoggedIn)
+  console.log(typeof(booleanisLoggedIn))
+   //if isLoggedIn value would have been diff data types
+   // 1-> true , 0-> false ,  ""-> false   , "chandan"-> true
+   
+
+  
