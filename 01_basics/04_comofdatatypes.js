fd8206fefb20d0bfa9e6433cf82b31cc56fc3503
letscode777 <163330053+letscode777@users.noreply.github.com>
2024-03-25 19:06:12 +0000

    changes

--- 01_basics/04_comofdatatypes.js
+++ 01_basics/04_comofdatatypes.js
@@ -13,4 +13,4 @@
 
 //equality check  vs comparison check are diffferent 
 // == equality check   involves comparison
-// === check data type and equality check  
+// === check data type and equality check  know te diff 
