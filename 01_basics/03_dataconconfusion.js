f7bde68ffe0c95a764ce2c214e07ef3608cb8b1f
letscode777 <163330053+letscode777@users.noreply.github.com>
2024-03-25 18:06:56 +0000

    changes

--- 01_basics/03_dataconconfusion.js
+++ 01_basics/03_dataconconfusion.js
@@ -21,4 +21,45 @@
    // 1-> true , 0-> false ,  ""-> false   , "chandan"-> true
    
 
+   // *************** operations ***************
+// let value = 4
+// let negvalue = -value
+// console.log(negvalue)
+
+//  console.log(2+2)  
+//  console.log(2-2)
+//  console.log(2*2 )
+//  console.log(2**3)  2 raise the power
+//  console.log(2/2)   
+//  console.log(2%3)   remainder  
+
+// let str1 = "hello"
+// let str2 = " chandan"
+// let str3 = str1 + str2 
+// console.log(str3)
+
+
+// console.log("1"+ 2)
+// console.log(1 + "2")
+// console.log("1"+2+2)
+// console.log(1 +1+"2")
+
+// console.log( (3+4) * (5 /3))  **use pelenthises for better code understanding  ORRR  GO TO TYPE CONVERSION OF ECMA SCRIPT 
+ 
+let gamecounter = 1000;
+gamecounter++ ;// ++gamecounter can be used but know the diff between prefic and suffix
+console.log(gamecounter);
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+
+
   
