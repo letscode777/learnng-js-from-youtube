024221837d36dfbec14deac883568640b228ac20
letscode777 <163330053+letscode777@users.noreply.github.com>
2024-03-30 17:50:13 +0000

    arraynotes

--- 01_basics/01_variables.js
+++
@@ -1,7 +0,0 @@
-/* const account_id = 2181
-let accountmail = "chaxbkabca@mail.com"
-var accountpass = "ajscbi277e2"
-accountcity = "jaipur"
-
-console.table([account_id,accountmail,accountpass,accountcity])
-*/
